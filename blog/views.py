ec519d5a1a3a3513f7c06b5c681eaf73e817c6bf
chawonseok <ckdnjstjr3@naver.com>
2020-02-12 15:40:44 +0900

    new -> now

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_data__lte=timezone.new().order_by('published_date'))
+        published_data__lte=timezone.now().order_by('published_date'))
     return render(request, 'blog/post_list.html', {'posts': posts})
